33ef517f86b6d348e29c18c7c6e40ad97e79616e
streetbz <kian.behzad@gmail.com>
2018-07-22 20:12:40 +0430

    implementation of gotopoint skill

--- Parsian/skills/gotopoint.cpp
+++ Parsian/skills/gotopoint.cpp
@@ -3,19 +3,17 @@
 #include "../util/bangbang.h"
 #include "../util/pid.h"
 
-void Soccer::gotoPoint(Robot* robot, int id, const rcsc::Vector2D&  targetPos, const rcsc::Vector2D& targetVel, double pidDist1, double pidDist2) {
-    double pidV = posPID[id].run(wm->ourRobot(id).pos.dist(targetPos));
-    double pidW = angPID[id].run(wm->ourRobot(id).th - targetVel.th().radian());
-
-    double bbV;
-    bangBangSpeed(wm->ourRobot(id).pos, wm->ourRobot(id).vel, targetPos, targetVel, bbV);
-    
-    double V, W;
-    if (wm->ourRobot(id).pos.dist(targetPos) < pidDist1) {
-        double t = (wm->ourRobot(id).pos.dist(targetPos) - pidDist2) / pidDist1;
-        t = std::fmax(t, 0);
-        V = (t * bbV) + (1 - t) * pidV;
-    }
-    
+void Soccer::gotoPoint(int id, const rcsc::Vector2D&  targetPos, const rcsc::Vector2D& targetVel) {
+	double dist = wm->ourRobot(id).pos.dist(targetPos);
+	double pathTh = -(targetPos - wm->ourRobot(id).pos).th().degree();
+	if (dist < 3) {
+		setRobotAng(id, targetVel.th().degree());
+	}
+	else if (fabs(wm->ourRobot(id).th - pathTh) > 15) {
+		setRobotAng(id, pathTh);
+	}
+	else {
+		setRobotPos(id, targetPos.length());
+	}
 
 }
