fe423f98c4b8d12675573806a6197823d17a8fb2
streetbz <kian.behzad@gmail.com>
2018-08-10 06:16:05 +0430

    reduce spin margin for playmake

--- Parsian/playmake.cpp
+++ Parsian/playmake.cpp
@@ -3,10 +3,10 @@
 
 
 void Soccer::PlayMake(int id) {
-	double margin{10};
+	double margin{20};
 	/*if (wm->ourRobot(id).vel.length() > 2)
 		margin = 17;*/
-	if (rcsc::Rect2D{ Field::width / 2 - Field::penaltyBwidth - margin, -Field::penaltyBheight / 2 - margin, Field::penaltyBwidth, Field::penaltyBheight + margin }.contains(wm->getBall().pos)) {
+	if (rcsc::Rect2D{ Field::width / 2 - Field::penaltyAwidth - margin, -Field::penaltyAheight / 2 - margin, Field::penaltyAwidth, Field::penaltyAheight + margin }.contains(wm->getBall().pos)) {
 		spin(id, Field::oppGoal(), 40);
 	}
 	else if (Field::ourPenaltyBRect().contains(wm->getBall().pos)) {
