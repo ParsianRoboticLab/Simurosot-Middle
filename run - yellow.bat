335dfb4c3a321b470336b621780c9ceea3942391
streetbz <kian.behzad@gmail.com>
2018-11-30 21:38:02 +0330

    remove consoleDebuger from batch files and rename them

--- run - yellow.bat
+++ run - yellow.bat
@@ -1,11 +1,8 @@
 @echo off
 tasklist /FI "IMAGENAME eq WorldModel.exe" 2>NUL | find /I /N "WorldModel.exe">NUL
 if "%ERRORLEVEL%"=="0" TaskKill /F /IM WorldModel.exe
-tasklist /FI "IMAGENAME eq ConsoleDebugger.exe" 2>NUL | find /I /N "ConsoleDebugger.exe">NUL
-if "%ERRORLEVEL%"=="0" TaskKill /F /IM ConsoleDebugger.exe
 Rem Move Parsian Simurosot dll auto
 echo "Hello There"
 COPY Release\Parsian.dll ..\Strategy4Yellow.dll
 echo "Done"
-START ..\Simurosot-master-1\ConsoleDebugger\Debug\ConsoleDebugger.exe
 START ../WorldModel.exe
